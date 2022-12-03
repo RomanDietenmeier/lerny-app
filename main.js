62779df63dd4e7b584b66fa3ab272c2d6097851b
Roman Dietenmeier <roman@d-robotic.de>
2022-12-03 01:21:20 +0100

    minimal electron example monaco-editor

---
+++ main.js
@@ -0,0 +1,28 @@
+const electron = require('electron');
+const app = electron.app;
+const BrowserWindow = electron.BrowserWindow;
+
+let mainWindow;
+
+app.whenReady().then(() => {
+    mainWindow = new BrowserWindow({
+        show: false,
+        webPreferences: { worldSafeExecuteJavaScript: true, devTools: true }
+    });
+    mainWindow.loadURL(`file://${__dirname}/dist/index.html`);
+    mainWindow.maximize();
+    mainWindow.show();
+    mainWindow.webContents.openDevTools();
+});
+
+app.on('window-all-closed', function () {
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
+});
+
+app.on('activate', function () {
+    if (mainWindow === null) {
+        createWindow();
+    }
+});
