10c7931ed81cfb3c756243b71a2e9399b2148b03
Roman Dietenmeier <roman@d-robotic.de>
2022-12-23 22:14:58 +0100

    Fixed monaco-editor not loading fonts.

--- webpack.config.js
+++ webpack.config.js
@@ -41,7 +41,7 @@ module.exports = {
             },
             {
                 test: /\.ttf$/,
-                use: ['file-loader']
+                type: 'asset/resource'
             }
         ]
     },
