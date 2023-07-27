cbb5b4a4c8ae4846450e5005e94caf8972762c0b
niman <nimanro@gmail.com>
2023-07-27 17:38:01 +0200

    LA-15 Add helpers to translate content

--- src/utilities/helper.ts
+++ src/utilities/helper.ts
@@ -1,3 +1,5 @@
+import { editor } from 'monaco-editor';
+
 export function sliceObjectInTwo<T>(obj: object): [T, T] {
   const entries = Object.entries(obj);
   const halfMark = Math.ceil(entries.length / 2);
@@ -38,4 +40,44 @@ export function stringFromBinary(binary: string): string {
     result += String.fromCharCode(char);
   });
   return result;
+}
+
+export const enum ChunkType {
+  Markdown,
+  WebComponent,
+}
+export type ContentChunk = {
+  type: ChunkType;
+  content: string;
+};
+export function transformContentToChunks(content: string): Array<ContentChunk> {
+  const componentPattern =
+    /(<executable-code-editor[\S\s]*<\/executable-code-editor>)|(<code-editor[\S\s]*<\/code-editor>)/gm;
+
+  const splits = content.split(componentPattern);
+
+  const chunks = splits
+    .filter((split) => split !== undefined)
+    .map((split) => split.trim());
+
+  const result: Array<ContentChunk> = [];
+  for (const chunk of chunks) {
+    result.push({
+      type: chunk.match(componentPattern)
+        ? ChunkType.WebComponent
+        : ChunkType.Markdown,
+      content: chunk,
+    });
+  }
+
+  return result;
+}
+
+export function getContentFromEditors(
+  editors: Array<editor.IStandaloneCodeEditor | undefined>
+): string {
+  const contentArray = editors.map((editor) =>
+    !editor ? '' : editor.getValue()
+  );
+  return contentArray.join('\r\n\r\n');
 }
