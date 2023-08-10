3881416eb0f249e1a37d7ef383cf709832b080e4
niman <nimanro@gmail.com>
2023-08-10 08:42:01 +0200

    LA-15 Adapt whiteTheme

--- src/styles/defaultTheme.ts
+++ src/styles/defaultTheme.ts
@@ -12,9 +12,9 @@ export const defaultTheme: DefaultTheme = {
 
 export const whiteTheme: DefaultTheme = {
   backgroundMain: '#fff',
-  backgroundSecondary: '#ccc',
-  titlebar: '#404040',
-  titlebarHover: '#505050',
+  backgroundSecondary: '#fff',
+  titlebar: '#505050',
+  titlebarHover: '#404040',
   primary: '#1a1a1a',
   primaryDisabled: '#1a1a1a',
   separatorColor: 'rgba(0, 0, 0, 0.33)',
